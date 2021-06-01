ed36d2474041955558aad73f34809064955e7ecf
snymanc <claytonsnyman@gmail.com>
2021-06-01 07:59:17 +0200

    Separating functionality into sub-commands

--- cli.py
+++ cli.py
@@ -1,13 +1,13 @@
 import click
 import requests
 
-# json = requests.get('http://api.openweathermap.org/data/2.5/weather?q=london&appid=c36164e51783d1af218c9b21e554625b').json()
-# print(json['weather'][0]['description'])
 
-SAMPLE_API_KEY = 'c36164e51783d1af218c9b21e554625b'
+@click.group()
+def main():
+    pass
 
 
-def current_weather(location, api_key=SAMPLE_API_KEY):
+def current_weather(location, api_key):
     url = 'http://api.openweathermap.org/data/2.5/weather'
 
     query_params = {
@@ -20,19 +20,22 @@ def current_weather(location, api_key=SAMPLE_API_KEY):
     return response.json()['weather'][0]['description']
 
 
-@click.command()
+@main.command()
 @click.argument('location')
 @click.option(
     '--api-key', '-a',
+    envvar="API_KEY",
     help='your API key for the OpenWeatherMap API',
 )
-def main(location, api_key):
+def current(location, api_key):
     """
     A little weather tool that shows you the current weather in a LOCATION of
     your choice. Provide the city name and optionally a two-digit country code.
     Here are two examples:
     1. London,UK
+
     2. Johannesburg
+
     You need a valid API key from OpenWeatherMap for the tool to work. You can
     sign up for a free account at https://openweathermap.org/appid.
     """
